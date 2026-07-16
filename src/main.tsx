99c00b1b5ec97b1e3461481ae4973f9dc3a64a45
ezekiel-charo <ezekiel3499@gmail.com>
2026-07-16 07:42:16 +0300

    Delete unused css

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    {/* <App /> */}
-  </StrictMode>,
-)
+  <StrictMode>{/* <App /> */}</StrictMode>
+);
