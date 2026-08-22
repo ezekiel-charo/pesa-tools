4a511cfcbcff5589e5c83248813816a7795c8239
ezekiel-charo <ezekiel3499@gmail.com>
2026-08-22 05:35:16 +0300

    Create shared header and footer components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="mt-20 border-t border-gray-200 py-3 flex flex-col items-center gap-3">
+      <div className="text-gray-600 font-medium">
+        &copy; 2026, Ezekiel Charo - All rights reserved
+      </div>
+    </footer>
+  );
+}
