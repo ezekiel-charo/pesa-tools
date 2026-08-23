dc34404f22e6c31f1311305da93a402e897fe815
ezekiel-charo <ezekiel3499@gmail.com>
2026-08-24 02:37:09 +0300

    Update base path for deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/pesa-tools/', // Enables deployment on path at https://ezekiel-charo.github.io/pesa-tools
   plugins: [react(), tailwindcss()],
 });
