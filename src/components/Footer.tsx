36668d699ca65e2d78d1b9f730c951873f40aceb
ezekiel-charo <ezekiel3499@gmail.com>
2026-08-23 15:02:00 +0300

    Fix typo in footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="mt-12 px-12 border-t border-gray-200 py-3 flex justify-between text-xs text-[#62615f] font-medium">
-      <div>* Your data stays never leaves this device</div>
+      <div>* Your data never leaves this device</div>
       <div>&copy; 2026 Ezekiel Charo. All rights reserved</div>
     </footer>
   );
