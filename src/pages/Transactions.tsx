bad71a231c910b57a2c62f62b840257dc3b77669
ezekiel-charo <ezekiel3499@gmail.com>
2026-08-23 18:19:42 +0300

    Enhance Transactions table

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,6 +1,8 @@
 import { formatDate } from 'date-fns';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { db } from '../core/db';
+import { copyToClipboard, formatCurreny } from '../core/utils';
+import { DocumentDuplicateIcon } from '@heroicons/react/24/solid';
 
 export default function Transactions() {
   const transactions = useLiveQuery(() => {
@@ -9,27 +11,49 @@ export default function Transactions() {
 
   return (
     <>
-      <div className="rounded-lg border border-gray-100">
-        <table>
+      <div className="rounded-lg border border-[#cdd2d5] overflow-hidden">
+        <table className="w-full text-[#646566]">
           <thead>
-            <tr>
-              <th>Transaction No.</th>
-              <th>Completion Time</th>
-              <th>IN/OUT</th>
-              <th>Amount</th>
-              <th>Balance</th>
+            <tr className="text-xs text-black [&_th]:semibold [&_th]:text-start [&_th]:p-4 [&_th]:bg-[#f5f9fa] [&_th]:border-r-2 [&_th]:border-[#feffff]">
+              <th>TRANSACTION NO.</th>
+              <th>DATE</th>
+              <th>AMOUNT</th>
+              <th>BALANCE</th>
             </tr>
           </thead>
           <tbody>
             {transactions?.map((transaction) => (
-              <tr key={transaction.id}>
-                <td>{transaction.transactionNo}</td>
-                <td>
-                  {formatDate(transaction.completionTime, 'dd/MM/yyy HH:mm:ss')}
+              <tr
+                key={transaction.id}
+                className="border-b border-gray-200 text-sm [&_td]:semibold [&_td]:text-start [&_td]:py-3 [&_td]:px-4 [&_td]:bg-white [&_td]:border-r-2 [&_td]:border-[#feffff]"
+              >
+                <td className="font-semibold">
+                  {transaction.transactionNo}
+                  <button
+                    onClick={() => copyToClipboard(transaction.transactionNo)}
+                    className="ms-2 cursor-pointer"
+                  >
+                    <DocumentDuplicateIcon className="size-4" />
+                  </button>
                 </td>
-                <td>{transaction.paidIn ? 'IN' : 'OUT'}</td>
-                <td>{transaction.paidIn || transaction.withdrawn}</td>
-                <td>{transaction.balance}</td>
+                <td>
+                  {formatDate(transaction.completionTime, 'dd/MM/yyy')}
+                  <span className="ms-2 text-xs font-medium text-gray-400">
+                    {formatDate(transaction.completionTime, 'h:mm aa')}
+                  </span>
+                </td>
+                <td
+                  className={
+                    (transaction.paidIn ? 'text-green-700' : 'text-red-700') +
+                    ' text-end! font-semibold'
+                  }
+                >
+                  {(transaction.paidIn ? '+ ' : '- ') +
+                    formatCurreny(transaction.paidIn || transaction.withdrawn)}
+                </td>
+                <td className="text-end! font-semibold">
+                  {formatCurreny(transaction.balance)}
+                </td>
               </tr>
             ))}
           </tbody>
