6d22d5e30a8085b5d64d137acfdac89df0364ebe
ezekiel-charo <ezekiel3499@gmail.com>
2026-08-26 03:09:47 +0300

    Increase main layout width for better content display

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -6,7 +6,7 @@ export default function MainLayout() {
   return (
     <>
       <Header />
-      <main className="lg:max-w-3/5 mx-3 lg:mx-auto">
+      <main className="lg:max-w-4/5 mx-3 lg:mx-auto">
         <div className="flex border-b border-gray-200 lg:mt-8 *:py-2 *:px-4 text-sm font-medium">
           <NavLink
             to=""
