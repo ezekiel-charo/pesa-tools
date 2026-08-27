dcb7da4282df7092a13fada2cd7e9a1e7efb157f
ezekiel-charo <ezekiel3499@gmail.com>
2026-08-27 17:20:32 +0300

    Add export functionality

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,3 +1,6 @@
+import * as ExcelJS from 'exceljs';
+import type { MpesaTransaction } from '../types/mpesa-transaction';
+
 export function formatCurreny(value?: number | null): string {
   if (!value && value !== 0) return '';
   return new Intl.NumberFormat().format(Math.abs(value));
@@ -12,4 +15,38 @@ export function copyToClipboard(text: string): void {
     .catch(() => {
       console.error('Failed to copy text:', text);
     });
+}
+
+export async function exportToXlsx(transactions: MpesaTransaction[]) {
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet('Transactions');
+  worksheet.columns = [
+    { header: 'Transaction No.', key: 'transactionNo', width: 16 },
+    {
+      header: 'Completion Time',
+      key: 'completionTime',
+      width: 32,
+    },
+    { header: 'PAID_IN/PAID_OUT', key: 'cashFlowDirection', width: 8 },
+    { header: 'Amount', key: 'amount', width: 16 },
+    { header: 'Balance', key: 'balance', width: 12 },
+    { header: 'Details', key: 'details', width: 64 },
+  ];
+  worksheet.addRows(transactions);
+
+  const buffer = await workbook.xlsx.writeBuffer();
+  const blob = new Blob([buffer], {
+    type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+  });
+
+  // Trigger the browser download
+  const fileName = 'Mpesa-statement.xlsx';
+  const url = window.URL.createObjectURL(blob);
+  const anchor = document.createElement('a');
+  anchor.href = url;
+  anchor.download = fileName;
+  anchor.click();
+
+  // Clean up the URL object
+  window.URL.revokeObjectURL(url);
 }
--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,61 +1,80 @@
-import { formatDate } from 'date-fns';
-import { useLiveQuery } from 'dexie-react-hooks';
-import { db } from '../core/db';
-import { copyToClipboard, formatCurreny } from '../core/utils';
-import { DocumentDuplicateIcon } from '@heroicons/react/24/solid';
+import {
+  ArrowDownTrayIcon,
+  ChevronUpDownIcon,
+  FunnelIcon,
+} from '@heroicons/react/24/outline';
+import { useState } from 'react';
+import TransactionsList from '../components/TransactionsList';
+import { exportToXlsx } from '../core/utils';
+import type {
+  FilterParams,
+  PaginationParams,
+  SortParams,
+} from '../types/filter-params';
+import { getFilteredTransactions } from '../core/query';
+
+const DEFUALT_SORTING: SortParams = { by: 'completionTime', direction: 'DESC' };
+const DEFAULT_PAGINATION: PaginationParams = { pageNo: 1, pageSize: 10 };
 
 export default function Transactions() {
-  const transactions = useLiveQuery(() => {
-    return db.mpesaTransactions.toArray();
-  });
+  const [search, setSearch] = useState<string>();
+  const [filters, setFilters] = useState<FilterParams>({});
+  const [sort, setSort] = useState(DEFUALT_SORTING);
+  const [pagination, setPagination] = useState(DEFAULT_PAGINATION);
+
+  const exportTransactions = async () => {
+    const transactions = await getFilteredTransactions(
+      filters,
+      sort,
+      pagination,
+      search
+    ).toArray();
+
+    exportToXlsx(transactions);
+  };
 
   return (
     <>
+      <div className="flex items-center justify-between mb-3">
+        <input
+          type="search"
+          placeholder="Search names, transaction number, etc."
+          className="w-80 bg-white border border-gray-300 outline-green-600 rounded-lg py-2 px-4"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="flex items-center gap-2">
+          <button className="inline-flex items-center gap-1 border border-gray-300 text-sm text-primary py-2 px-4 rounded-lg font-medium cursor-pointer">
+            <FunnelIcon className="size-4" /> Filter
+          </button>
+          <button className="inline-flex items-center gap-1 border border-gray-300 text-sm text-primary py-2 px-4 rounded-lg font-medium cursor-pointer">
+            <ChevronUpDownIcon className="size-4" /> Sort
+          </button>
+          <button
+            onClick={exportTransactions}
+            className="inline-flex items-center gap-1 bg-primary text-sm text-white py-2 px-4 rounded-lg font-medium cursor-pointer"
+          >
+            <ArrowDownTrayIcon className="size-4" /> Export
+          </button>
+        </div>
+      </div>
       <div className="rounded-lg border border-[#cdd2d5] overflow-hidden">
         <table className="w-full text-[#646566]">
           <thead>
-            <tr className="text-xs text-black [&_th]:semibold [&_th]:text-start [&_th]:p-4 [&_th]:bg-[#f5f9fa] [&_th]:border-r-2 [&_th]:border-[#feffff]">
+            <tr className="text-xs text-black [&_th]:semibold [&_th]:text-start [&_th]:p-4 [&_th]:bg-[#f5f9fa] [&_th]:border-r-2 [&_th]:last:border-r-0 [&_th]:border-[#feffff]">
               <th>TRANSACTION NO.</th>
               <th>DATE</th>
+              <th>DETAILS</th>
               <th>AMOUNT</th>
               <th>BALANCE</th>
             </tr>
           </thead>
           <tbody>
-            {transactions?.map((transaction) => (
-              <tr
-                key={transaction.id}
-                className="border-b border-gray-200 text-sm [&_td]:semibold [&_td]:text-start [&_td]:py-3 [&_td]:px-4 [&_td]:bg-white [&_td]:border-r-2 [&_td]:border-[#feffff]"
-              >
-                <td className="font-semibold">
-                  {transaction.transactionNo}
-                  <button
-                    onClick={() => copyToClipboard(transaction.transactionNo)}
-                    className="ms-2 cursor-pointer"
-                  >
-                    <DocumentDuplicateIcon className="size-4" />
-                  </button>
-                </td>
-                <td>
-                  {formatDate(transaction.completionTime, 'dd/MM/yyy')}
-                  <span className="ms-2 text-xs font-medium text-gray-400">
-                    {formatDate(transaction.completionTime, 'h:mm aa')}
-                  </span>
-                </td>
-                <td
-                  className={
-                    (transaction.paidIn ? 'text-green-700' : 'text-red-700') +
-                    ' text-end! font-semibold'
-                  }
-                >
-                  {(transaction.paidIn ? '+ ' : '- ') +
-                    formatCurreny(transaction.paidIn || transaction.withdrawn)}
-                </td>
-                <td className="text-end! font-semibold">
-                  {formatCurreny(transaction.balance)}
-                </td>
-              </tr>
-            ))}
+            <TransactionsList
+              filters={filters}
+              sort={sort}
+              pagination={pagination}
+              search={search}
+            />
           </tbody>
         </table>
       </div>
